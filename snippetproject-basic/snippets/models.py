43aa8a44b4059acd6cab002f2a438847269ff6cc
Gabriele Marcantonio <gabri.marc@gmx.net>
2018-06-23 08:04:47 +0200

    Add files via upload

---
+++ snippetproject-basic/snippets/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Snippet(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    def body_preview(self):
+        return self.body[:50]
